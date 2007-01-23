dbdbc920732c4cb25ddae99243bd7798a03427fb
chucker23n <chucker23n@3d429dc8-b227-0410-8030-558227cecf10>
2007-01-23 10:55:15 +0000

    Initial commit

---
+++ trunk/IFHUDTextField+Shortcuts.h
@@ -0,0 +1,21 @@
+//
+//  IFHUDTextField+Shortcuts.h
+//  Twittereeze
+//
+//  Created by Sören Kuklau on 21/01/07.
+//  Copyright 2007 chucker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IFHUDBackground : NSView {}
+@end
+
+@interface IFHUDTableView : NSTableView
+@end
+
+@interface IFHUDTextField : NSTextField {}
+@end
+
+@interface IFHUDTextField (Shortcuts)
+@end
---
+++ trunk/IFHUDTextField+Shortcuts.m
@@ -0,0 +1,45 @@
+//
+//  IFHUDTextField+Shortcuts.m
+//  Twittereeze
+//
+//  Created by Sören Kuklau on 21/01/07.
+//  Copyright 2007 chucker. All rights reserved.
+//
+
+#import "IFHUDTextField+Shortcuts.h"
+
+@implementation IFHUDTextField (Shortcuts)
+- (void) keyUp: (NSEvent *) event {
+	if (! ([event modifierFlags] & NSControlKeyMask) )
+		return;
+	if (([event keyCode] < 125) || ([event keyCode] > 126))
+		return;
+
+	NSEnumerator * enumerator = [[[self superview] subviews] objectEnumerator];
+	id object;
+	IFHUDTableView * tableView = nil;
+
+	while (object = [enumerator nextObject])
+		if (([object isKindOfClass:[IFHUDBackground class]]) && ([[[[object subviews] objectAtIndex:0] subviews] count] > 0))
+			tableView = [[[[[[object subviews] objectAtIndex:0] subviews] objectAtIndex:0] subviews] objectAtIndex:0];
+
+	if (tableView == nil)
+		return;
+
+	if ([tableView numberOfRows] == 0)
+		return;
+
+	int row = [tableView selectedRow];
+
+	switch ([event keyCode]) {
+	case 125: // down arrow key
+		row++; // one row lower
+		[tableView selectRowIndexes:[NSIndexSet indexSetWithIndex:row] byExtendingSelection:NO];
+		break;
+	case 126: // up arrow key
+		row--; // one row higher
+		[tableView selectRowIndexes:[NSIndexSet indexSetWithIndex:row] byExtendingSelection:NO];
+		break;
+	}
+}
+@end
