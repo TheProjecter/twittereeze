8649b2300e4f2fda2ef465f426f610956d85222c
chucker23n <chucker23n@3d429dc8-b227-0410-8030-558227cecf10>
2007-01-23 10:57:36 +0000

    Tagging 1.0

---
+++ tags/1.0/Twittereeze.m
@@ -0,0 +1,50 @@
+//
+//  Twittereeze.m
+//  Twittereeze
+//
+//  Created by Sören Kuklau on 16/11/06.
+//  Copyright 2006 chucker. All rights reserved.
+//
+
+#import "Twittereeze.h"
+
+@implementation Twittereeze
+BOOL DTRenameSelector(Class _class, SEL _oldSelector, SEL _newSelector)
+{
+	Method method = nil;
+
+	// First, look for the methods
+	method = class_getInstanceMethod(_class, _oldSelector);
+	if (method == nil)
+		return NO;
+	
+	method->method_name = _newSelector;
+	return YES;
+}
+
+/**
+ * A special method called by SIMBL once the application has started and all classes are initialized.
+ */
++ (void) load
+{
+	Twittereeze * twittereeze = [Twittereeze sharedInstance];
+
+	id swizzle_nsap = [NSApplication class];
+	DTRenameSelector(swizzle_nsap, @selector(sendEvent:), @selector (_apple_sendEvent:));
+	DTRenameSelector(swizzle_nsap, @selector(_twittereeze_sendEvent:), @selector(sendEvent:));
+//	NSLog(@"Twittereeze is running.");
+}
+
+/**
+ * @return the single static instance of the plugin object
+ */
++ (Twittereeze *) sharedInstance
+{
+	static Twittereeze * twittereeze = nil;
+
+	if (twittereeze == nil)
+		twittereeze = [[Twittereeze alloc] init];
+
+	return twittereeze;
+}
+@end
