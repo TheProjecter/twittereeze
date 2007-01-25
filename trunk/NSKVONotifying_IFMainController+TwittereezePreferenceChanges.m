3ad0362e91dfe04731b1103c96e099fa749c5deb
chucker23n <chucker23n@3d429dc8-b227-0410-8030-558227cecf10>
2007-01-25 16:17:13 +0000

    Badgerbadgerbad... well, we added a badge. Credit to amon-re *:)
    Now, as for more serious and useful additions. ;)

--- trunk/NSKVONotifying_IFMainController+TwittereezePreferenceChanges.m
+++ trunk/NSKVONotifying_IFMainController+TwittereezePreferenceChanges.m
@@ -10,8 +10,34 @@
 
 @implementation NSKVONotifying_IFMainController (TwittereezePreferenceChanges)
 - (IBAction) _twittereeze_showPreferenceWindow: (id) sender {
-	NSLog(@"before");
+	NSEnumerator * enumerator = [[NSApp windows] objectEnumerator];
+	id object;
+	id notificationWindow = nil;
+	id preferenceSheet = nil;
+
+	while (object = [enumerator nextObject])
+		if ([object isKindOfClass:[IFHUDWindow class]])
+			notificationWindow = object;
+
 	[self _twitterrific_showPreferenceWindow:sender];
-	NSLog(@"after");
+
+	if (notificationWindow != nil)
+		preferenceSheet = [notificationWindow attachedSheet];
+
+	enumerator = [[[preferenceSheet contentView] subviews] objectEnumerator];
+
+	id twitterrificBannerImageView = nil;
+	while (object = [enumerator nextObject])
+		if (([object isKindOfClass:[NSImageView class]]) && (! [object isKindOfClass:[IFClickableImageView class]]))//[[object image] size].width == 340.0))
+			twitterrificBannerImageView = object;
+
+	if (twitterrificBannerImageView != nil) {
+		NSRect twittereezeBadgeImageFrame = [twitterrificBannerImageView frame];
+		NSImage * twittereezeBadgeImage = [[NSImage alloc] initWithContentsOfFile:
+			[[NSBundle bundleForClass:[Twittereeze class]] pathForResource:@"Twittereeze_badge" ofType:@"png"]];
+		NSImageView * twittereezeBadgeImageView = [[NSImageView alloc] initWithFrame:twittereezeBadgeImageFrame];
+		[twittereezeBadgeImageView setImage:twittereezeBadgeImage];
+		[[preferenceSheet contentView] addSubview:twittereezeBadgeImageView];
+	}
 }
 @end
