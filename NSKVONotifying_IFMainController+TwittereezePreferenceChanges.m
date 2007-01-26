a7e8b428c5656281045987ab1721a22ca4c0eeeb
chucker23n <chucker23n@3d429dc8-b227-0410-8030-558227cecf10>
2007-01-26 06:37:16 +0000

    Clean-up

--- NSKVONotifying_IFMainController+TwittereezePreferenceChanges.m
+++ NSKVONotifying_IFMainController+TwittereezePreferenceChanges.m
@@ -54,39 +54,22 @@
 			twitterrificBannerImageView = object;
 	}
 
-	if (twitterrificBannerImageView != nil) {
-		NSImage * twittereezeBadgeImage = [[NSImage alloc] initWithContentsOfFile:
-			[[NSBundle bundleForClass:[Twittereeze class]] pathForResource:@"Twittereeze_badge" ofType:@"png"]];
-		NSImageView * twittereezeBadgeImageView = [[NSImageView alloc] initWithFrame:[twitterrificBannerImageView frame]];
-		[twittereezeBadgeImageView setImage:twittereezeBadgeImage];
-		[twittereezeBadgeImageView setTag:TwittereezeBadgeImageViewTag];
-		[[preferenceSheet contentView] addSubview:twittereezeBadgeImageView];
+	if (twitterrificBannerImageView != nil)
+		[self _twittereeze_addBadgeImageToView:[preferenceSheet contentView]
+			onTopOfBannerImageViewFrame:[twitterrificBannerImageView frame]];
+
 //
 //		[preferenceSheet setContentView:twitterrificBannerImageView];
 //		[preferenceSheet setFrame:[twitterrificBannerImageView frame] display:YES animate:YES];
-	}
 
-	NSTextField * copyrightTextField = [[NSTextField alloc] initWithFrame:NSMakeRect(17.0, 55.0, 188.0, 11.0)];
-	[copyrightTextField setBezeled:NO]; [copyrightTextField setBordered:NO];
-//	[copyrightTextField setAlignment:NSRightTextAlignment];
-	[copyrightTextField setDrawsBackground:NO];
-	[copyrightTextField setTextColor:[[NSColor blackColor] blendedColorWithFraction:0.75 ofColor:[NSColor whiteColor]]];
-	[copyrightTextField setEditable:NO]; [copyrightTextField setEnabled:NO];
-	[copyrightTextField setFont:[NSFont systemFontOfSize:[NSFont systemFontSizeForControlSize:NSMiniControlSize]]];
-	[copyrightTextField setStringValue:[@"Twittereeze Copyright " stringByAppendingString:[[NSBundle bundleForClass:[Twittereeze class]]
-		localizedStringForKey:@"NSHumanReadableCopyright" value:nil table:@"InfoPlist"]]];
-	[[preferenceSheet contentView] addSubview:copyrightTextField];
+	[self _twittereeze_addCopyrightAndVersionTextFieldsToView:[preferenceSheet contentView]];
 
-	NSTextField * versionTextField = [[NSTextField alloc] initWithFrame:NSMakeRect(207.0, 55.0, 116.0, 11.0)];
-	[versionTextField setBezeled:NO]; [versionTextField setBordered:NO];
-	[versionTextField setAlignment:NSRightTextAlignment];
-	[versionTextField setDrawsBackground:NO];
-	[versionTextField setTextColor:[[NSColor blackColor] blendedColorWithFraction:0.75 ofColor:[NSColor whiteColor]]];
-	[versionTextField setEditable:NO]; [versionTextField setEnabled:NO];
-	[versionTextField setFont:[NSFont systemFontOfSize:[NSFont systemFontSizeForControlSize:NSMiniControlSize]]];
-	[versionTextField setStringValue:[@"Version " stringByAppendingString:[[[NSBundle bundleForClass:[Twittereeze class]]
-		infoDictionary] objectForKey:@"CFBundleVersion"]]];
-	[[preferenceSheet contentView] addSubview:versionTextField];
+
+//		[[preferenceSheet contentView] setTag:TwittereezePreferenceSheetContentViewTag];
+
+//	NSLog(@"%f %f", [preferenceSheet frame].size.height, [[preferenceSheet contentView] frame].size.height);
+//	[[preferenceSheet contentView] frame].origin.x=50.0;
+
 
 //	NSLog(@"%f %f", [preferenceSheet frame].size.height, [[preferenceSheet contentView] frame].size.height);
 
@@ -94,10 +77,40 @@
 	preferenceSheetFrame.size.height += (11.0 + 8.0);
 	preferenceSheetFrame.origin.y -= (11.0 + 8.0);
 	[preferenceSheet setFrame:preferenceSheetFrame display:YES animate:YES];
-//		[[preferenceSheet contentView] setTag:TwittereezePreferenceSheetContentViewTag];
+}
 
-//	NSLog(@"%f %f", [preferenceSheet frame].size.height, [[preferenceSheet contentView] frame].size.height);
-//	[[preferenceSheet contentView] frame].origin.x=50.0;
+- (void) _twittereeze_addBadgeImageToView: (NSView *) view onTopOfBannerImageViewFrame: (NSRect) twitterrificBannerImageViewFrame {
+	NSImage * twittereezeBadgeImage = [[NSImage alloc] initWithContentsOfFile:
+		[[NSBundle bundleForClass:[Twittereeze class]] pathForResource:@"Twittereeze_badge" ofType:@"png"]];
+	NSImageView * twittereezeBadgeImageView = [[NSImageView alloc] initWithFrame:twitterrificBannerImageViewFrame];
+	[twittereezeBadgeImageView setImage:twittereezeBadgeImage];
+	[twittereezeBadgeImageView setTag:TwittereezeBadgeImageViewTag];
+	[view addSubview:twittereezeBadgeImageView];
+}
 
+- (void) _twittereeze_addCopyrightAndVersionTextFieldsToView: (NSView *) view {
+	NSFont * miniControlFont = [NSFont systemFontOfSize:[NSFont systemFontSizeForControlSize:NSMiniControlSize]];
+	NSColor * disabledTextColor = [[NSColor blackColor] blendedColorWithFraction:0.85 ofColor:[NSColor whiteColor]];
+
+	NSTextField * copyrightTextField = [[NSTextField alloc] initWithFrame:NSMakeRect(17.0, 55.0, 188.0, 11.0)];
+	[copyrightTextField setBezeled:NO]; [copyrightTextField setBordered:NO];
+	[copyrightTextField setDrawsBackground:NO];
+	[copyrightTextField setTextColor:disabledTextColor];
+	[copyrightTextField setEditable:NO]; [copyrightTextField setEnabled:NO];
+	[copyrightTextField setFont:miniControlFont];
+	[copyrightTextField setStringValue:[@"Twittereeze Copyright " stringByAppendingString:[[NSBundle bundleForClass:[Twittereeze class]]
+		localizedStringForKey:@"NSHumanReadableCopyright" value:nil table:@"InfoPlist"]]];
+	[view addSubview:copyrightTextField];
+
+	NSTextField * versionTextField = [[NSTextField alloc] initWithFrame:NSMakeRect(207.0, 55.0, 116.0, 11.0)];
+	[versionTextField setBezeled:NO]; [versionTextField setBordered:NO];
+	[versionTextField setAlignment:NSRightTextAlignment];
+	[versionTextField setDrawsBackground:NO];
+	[versionTextField setTextColor:disabledTextColor];
+	[versionTextField setEditable:NO]; [versionTextField setEnabled:NO];
+	[versionTextField setFont:miniControlFont];
+	[versionTextField setStringValue:[@"Version " stringByAppendingString:[[[NSBundle bundleForClass:[Twittereeze class]]
+		infoDictionary] objectForKey:@"CFBundleVersion"]]];
+	[view addSubview:versionTextField];
 }
 @end
